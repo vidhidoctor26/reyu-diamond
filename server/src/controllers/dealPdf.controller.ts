7a4ad32a4a8047b5397d92b7e329ca452e9c4c56
vidhidoctor26 <vidhidoctor@logicgoinfotech.in>
2026-07-27 00:04:50 +0530

    commit

--- server/src/controllers/dealPdf.controller.ts
+++ server/src/controllers/dealPdf.controller.ts
@@ -4,7 +4,8 @@ import { sendResponse, SuccessCode, SUCCESS_MESSAGES } from "../utils";
 
 export const generateDealPdf = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const result = await generateAndUploadDealPdf(req.params.dealId);
+    const dealId = Array.isArray(req.params.dealId) ? req.params.dealId[0] : (req.params.dealId as string);
+    const result = await generateAndUploadDealPdf(dealId);
     return sendResponse(res, 200, true, SUCCESS_MESSAGES[SuccessCode.DEAL_PDF_GENERATED], result, undefined, SuccessCode.DEAL_PDF_GENERATED);
   } catch (error) {
     next(error);
